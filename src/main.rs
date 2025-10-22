2f31bf347e4840f975415050e28a8a18853898a8
Aidan Holm <aidanholm@gmail.com>
2025-10-22 23:16:13 +0800

    Add an initial HTTP server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, response::Html};
+
+async fn handle_index() -> Result<Html<String>, (StatusCode, String)> {
+    Ok(Html("Hello!".to_string()))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/", axum::routing::get(handle_index));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:7743")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
